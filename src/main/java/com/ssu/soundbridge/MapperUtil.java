2393e448edab630aac477a6a419a07b14802d182
isaac105 <hmkim735@gmail.com>
2025-07-07 22:29:42 +0900

    Fix: Remove Cached files

--- src/main/java/com/ssu/soundbridge/MapperUtil.java
+++
@@ -1,5 +0,0 @@
-package com.yourdomain.soundbridge.util;
-
-public class MapperUtil {
-    // 예시: 추후 공통 매핑 로직 추가 가능
-} 
--- src/main/java/com/ssu/soundbridge/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yourdomain.soundbridge.repository;
-
-import com.yourdomain.soundbridge.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-} 
